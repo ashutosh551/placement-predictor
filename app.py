5740b66ee2c4211679a2a82e7d6d71aea40a0a7f
ashutosh551 <96560585+ashutosh551@users.noreply.github.com>
2022-10-20 12:08:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('./model.pkl','rb'))
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('./index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict_placement():
@@ -23,7 +23,7 @@ def predict_placement():
     else:
         result = 'not placed'
 
-    return render_template('index.html',result=result)
+    return render_template('./index.html',result=result)
 
 
 if __name__ == '__main__':
